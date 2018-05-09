9d86002cb0440a7a6db13041de7caa5755b6252d
Superunknown1103 <superunknown1103@gmail.com>
2018-05-09 08:29:26 -0500

    login works

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+      
     $('#completed').click(function(){
         var password = $('#newPass').val();
         var email = $('#email').val();
@@ -9,7 +9,17 @@ $(document).ready(function(){
             alert('Please fill out all fields!');
         }
         else {
-            alert('Good job!')
+            if (password !== password2){
+                alert('Passwords do not match!')
+            } else {
+                alert('User registration was sucessful.');
+                firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error){
+                    // Handle Errors Here
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    alert(error.message);
+                });
+            }
     }
 });
 
